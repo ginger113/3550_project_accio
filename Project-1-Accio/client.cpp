994beb8638d7d3fc9627487eab0d0ec0fce6baa6
Chris Fuchs <cfuchs@unomaha.edu>
2021-04-01 22:04:20 -0500

    updated client.cpp

--- Project-1-Accio/client.cpp
+++ Project-1-Accio/client.cpp
@@ -8,78 +8,68 @@
 #include <unistd.h>
 
 #include <iostream>
-#include <sstream>
+#include <fstream>
+
+using namespace std;
 
 int
-main()
+main(int argc, char* argv[])
 {
+    int port = atoi(argv[2]);
+    char* fileName = argv[3];
+    
+    if (argc != 4) {
+        std::cerr << "ERROR: You  need 3 arguments.\n";
+        return 0;
+    }
+
+    if (port < 1024) {
+        std::cerr << "ERROR: incorrect port number\n";
+        exit(1);
+    }
+
   // create a socket using TCP IP
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-  // struct sockaddr_in addr;
-  // addr.sin_family = AF_INET;
-  // addr.sin_port = htons(40001);     // short, network byte order
-  // addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  // memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
-  // if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-  //   perror("bind");
-  //   return 1;
-  // }
-
   struct sockaddr_in serverAddr;
   serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(40000);     // short, network byte order
+  serverAddr.sin_port = htons(port);     // short, network byte order
   serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
 
   // connect to the server
   if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-    perror("connect");
+      std::cerr << "ERROR: connect\n";
     return 2;
   }
 
   struct sockaddr_in clientAddr;
   socklen_t clientAddrLen = sizeof(clientAddr);
   if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1) {
-    perror("getsockname");
+      std::cerr << "ERROR: getsockname\n";
     return 3;
   }
 
   char ipstr[INET_ADDRSTRLEN] = {'\0'};
   inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
-  std::cout << "Set up a connection from: " << ipstr << ":" <<
-    ntohs(clientAddr.sin_port) << std::endl;
 
 
   // send/receive data to/from connection
-  bool isEnd = false;
-  std::string input;
-  char buf[20] = {0};
-  std::stringstream ss;
+  fstream file;
+  file.open(fileName, ios::in | ios::binary);
+  if (!file.is_open()) {
+      std::cerr << "ERROR: can't load file\n";
+      exit(EXIT_FAILURE);
+  }
+  std::string contents( (std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
-  while (!isEnd) {
-    memset(buf, '\0', sizeof(buf));
-
-    std::cout << "send: ";
-    std::cin >> input;
-    if (send(sockfd, input.c_str(), input.size(), 0) == -1) {
-      perror("send");
-      return 4;
-    }
-
-
-    if (recv(sockfd, buf, 20, 0) == -1) {
-      perror("recv");
-      return 5;
-    }
-    ss << buf << std::endl;
-    std::cout << "echo: ";
-    std::cout << buf << std::endl;
-
-    if (ss.str() == "close\n")
-      break;
-
-    ss.str("");
+  int total = 0;
+  int bytesleft = contents.length();
+  int bytes_sent = 0;
+  while (total < contents.length()) {
+      bytes_sent = send(sockfd, contents.c_str() + total, bytesleft, 0);
+      total += bytes_sent;
+      bytesleft -= bytes_sent;
   }
 
   close(sockfd);
